0ecd972a2364cdff4abd61354cf1ac6f175b162c
Paulo Rabelo <paulorabelo@hotmail.com>
2022-01-20 13:45:23 -0300

    Desenvolvimento dos layouts de listagem e detalhe de partidas

---
+++ app/src/main/java/br/com/mrgsoft/simulator/DetailActivity.kt
@@ -0,0 +1,20 @@
+package br.com.mrgsoft.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.mrgsoft.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
